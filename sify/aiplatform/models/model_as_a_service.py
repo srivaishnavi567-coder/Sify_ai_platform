ae242ff3faea15dc448875f7d04a398f899a8cff
srivaishnavi567-coder <srivaishnavi567@gmail.com>
2026-01-06 11:34:40 +0530

    model gen SDK with Langfuse

--- sify/aiplatform/models/model_as_a_service.py
+++ sify/aiplatform/models/model_as_a_service.py
@@ -110,7 +110,7 @@ class ModelAsAService:
         if file is None:
             raise ValueError("File must be provided")
 
-        span = self.tracer.start_span("maas.speech_to_text", {"model": self.model_id})
+        span = self.tracer("maas.speech_to_text", {"model": self.model_id})
 
         try:
             response = self._send_request(
@@ -139,7 +139,7 @@ class ModelAsAService:
         if file is None:
             raise ValueError("File must be provided")
 
-        span = self.tracer.start_span("maas.audio_translation", {"model": self.model_id})
+        span = self.tracer("maas.audio_translation", {"model": self.model_id})
 
         try:
             response = self._send_request(
@@ -167,7 +167,7 @@ class ModelAsAService:
     def text_to_speech(self, input_text: str, voice: str, **kwargs) -> bytes:
         self._validate_required_params({"input_text": input_text, "voice": voice})
 
-        span = self.tracer.start_span("maas.text_to_speech", {"model": self.model_id})
+        span = self.tracer("maas.text_to_speech", {"model": self.model_id})
 
         try:
             audio = self._send_request(
@@ -205,7 +205,7 @@ class ModelAsAService:
 
         self._validate_required_params({"input": input_data})
 
-        span = self.tracer.start_span("maas.embeddings", {"model": self.model_id})
+        span = self.tracer("maas.embeddings", {"model": self.model_id})
 
         try:
             response = self._send_request(
@@ -255,7 +255,7 @@ class ModelAsAService:
             **kwargs,
         }
 
-        span = self.tracer.start_span("maas.chat_completion", data)
+        span = self.tracer("maas.chat_completion", data)
 
         if not stream:
             try:
@@ -333,7 +333,7 @@ class ModelAsAService:
             **kwargs,
         }
 
-        span = self.tracer.start_span("maas.completion", data)
+        span = self.tracer("maas.completion", data)
 
         if not stream:
             try:
