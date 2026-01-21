9026ef43074f1ba64c20dc2c227c5d2accae9e89
srivaishnavi567-coder <srivaishnavi567@gmail.com>
2026-01-21 16:19:49 +0530

    Update client.py

--- sify/aiplatform/observability/langfuse/client.py
+++ sify/aiplatform/observability/langfuse/client.py
@@ -1,11 +1,8 @@
 
 from typing import Optional
 from langfuse import Langfuse
-# from .config import get_langfuse_config
-# from .config import get_app_name
-from sify.aiplatform.observability.langfuse.config import (
-    get_langfuse_config
-)
+from sify.aiplatform.observability.langfuse.config import get_langfuse_config
+from sify.aiplatform.observability.langfuse.detect_app import detect_app_name
 _client: Optional[Langfuse] = None
 
 
@@ -18,11 +15,15 @@ def get_langfuse_client() -> Optional[Langfuse]:
 
     if _client:
         return _client
-
+    app_name = detect_app_name()
     _client = Langfuse(
         host=cfg.host,
         public_key=cfg.public_key,
         secret_key=cfg.secret_key,
+
+        metadata={
+            "app_name": app_name
+        }
     )
     return _client
 
